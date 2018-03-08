6150384e2c54d4e734eba6b460487892ad56673d
Julien M'Poy <julien.mpoy@he-arc.ch>
2018-03-08 18:59:09 +0100

    ANDROID_HOME is different depending on OS

--- shared-vars.sh
+++ shared-vars.sh
@@ -2,7 +2,12 @@
 export VAGRANT_DEFAULT_PROVIDER=libvirt
 
 # Android
-export ANDROID_HOME=${HOME}/Android/Sdk
+if [[ `uname` == 'Darwin' ]]
+then
+    export ANDROID_HOME=${HOME}/Library/Android/sdk
+else
+    export ANDROID_HOME=${HOME}/Android/Sdk
+fi
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
-export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+export ANDROID_SDK_ROOT=${ANDROID_HOME}
