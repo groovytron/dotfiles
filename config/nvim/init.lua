2be8cb273e1b260079e82ee0282c02f42017da3e
Julien M'Poy <julien.mpoy@gmail.com>
2023-02-07 15:34:59 +0100

    Add golang and Dockerfile support to neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -113,6 +113,42 @@ vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]
 vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
 vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
 
+-- Docker
+require('lspconfig').dockerls.setup{}
+
+-- Golang
+local util = require ("lspconfig/util")
+require('lspconfig').gopls.setup {
+  cmd = {"gopls", "serve"},
+  filetypes = {"go", "gomod"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
+
+function go_org_imports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+      end
+    end
+  end
+end
+
+
+vim.cmd([[autocmd BufWritePre *.go lua go_org_imports()]])
+
 -- Set up nvim-cmp.
 local cmp = require('cmp')
 
