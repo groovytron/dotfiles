636ba4d8ccc1cd60d620bf7351c402c17b558f94
Julien M'Poy <julien.mpoy@gmail.com>
2024-09-06 11:44:04 +0200

    Update tree-sitter config due to upgrade

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,11 +24,6 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     width = 30,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
   },
   renderer = {
     group_empty = false,
